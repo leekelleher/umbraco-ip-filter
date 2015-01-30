d6d6c2b2d10ff8a942e9d3e1b7b531ed17da1163
mattbrailsford <me@mattbrailsford.com>
2015-01-30 14:00:57 +0000

    Updated GetClientIpAddress to take port numbers into account, aswell as ::1 local address.

--- Src/Our.Umbraco.IpFilter/Extensions/HttpRequestExtensions.cs
+++ Src/Our.Umbraco.IpFilter/Extensions/HttpRequestExtensions.cs
@@ -1,23 +1,37 @@
-﻿using System.Web;
+﻿using System.Linq;
+using System.Web;
+using Umbraco.Core;
 
 namespace Our.Umbraco.IpFilter.Extensions
 {
     public static class HttpRequestExtensions
     {
-        public static string GetClientIpAddress(this HttpRequest req)
+        public static string GetClientIpAddress(this HttpRequest request)
         {
-            var ipAddress = req.ServerVariables["HTTP_X_FORWARDED_FOR"];
+            // check the querystring for a forced IP address (for testing purposes)
+            var ipAddress = request.QueryString["ip"];
 
-            if (!string.IsNullOrEmpty(ipAddress))
+            // check the "HTTP_X_FORWARDED_FOR" value, splits the list by comma, then take the first value
+            if (string.IsNullOrWhiteSpace(ipAddress))
             {
-                var addresses = ipAddress.Split(',');
-                if (addresses.Length != 0)
-                {
-                    return addresses[0];
-                }
+                var forwarded = request.ServerVariables["HTTP_X_FORWARDED_FOR"];
+                if (!string.IsNullOrEmpty(forwarded))
+                    ipAddress = forwarded.ToDelimitedList().FirstOrDefault();
             }
 
-            return req.ServerVariables["REMOTE_ADDR"];
+            // gets the fallback value (from "REMOTE_ADDR")
+            if (string.IsNullOrWhiteSpace(ipAddress))
+                ipAddress = request.ServerVariables["REMOTE_ADDR"];
+
+            // checks if the IP address is IPv6 localhost
+            if (ipAddress != null && ipAddress.Contains("::1"))
+                ipAddress = ipAddress.Replace("::1", "127.0.0.1");
+
+            // checks if the IP address contains a port number, then splits it by colon, taking the first value (IP address)
+            if (ipAddress != null && ipAddress.Contains(":"))
+                ipAddress = ipAddress.ToDelimitedList(":").FirstOrDefault();
+
+            return ipAddress;
         }
     }
 }
