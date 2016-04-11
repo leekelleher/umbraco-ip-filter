ac537dedcb5d143dfb6d87aa753b3d0b4bab41a8
leekelleher <leekelleher@gmail.com>
2016-04-11 14:33:44 +0100

    Updated AssemblyInfo

--- Src/Our.Umbraco.IpFilter/Properties/AssemblyInfo.cs
+++ Src/Our.Umbraco.IpFilter/Properties/AssemblyInfo.cs
@@ -1,36 +1,16 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Our.Umbraco.IpFilter")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("IP Address Filter for an Umbraco-powered (front-end) website")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("The Umbraco Community")]
 [assembly: AssemblyProduct("Our.Umbraco.IpFilter")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright \xa9 The Umbraco Community 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
+[assembly: Guid("BFF874FF-9D41-4E49-8A7E-92FCCC7D1838")]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("bff874ff-9d41-4e49-8a7e-92fccc7d1838")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.*")]
